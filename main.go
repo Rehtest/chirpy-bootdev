5bdeea9d0e6a989384091b2bbce8547ab661089c
RehanGeorge <rehan.george@gmail.com>
2025-09-25 11:46:23 +0530

    Add healthz endpoint

--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	// Step 1: Set Content-Type
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	// Step 2: Write status code
+	w.WriteHeader(http.StatusOK)
+
+	// Step 3: Write body
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	fmt.Println("Hello, Chirpy Bootdev!")
 
@@ -15,8 +26,11 @@ func main() {
 	// Step 2: Use the http.FileServer to serve static files from the current directory
 	fileServer := http.FileServer(http.Dir("."))
 
-	// Step 3: Use Handle() method to add a handler for the root path "/"
-	mux.Handle("/", fileServer)
+	// Use StripPrefix so that /app/index.html resolves to ./index.html
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+
+	// Step 3: Add readiness endpoint
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
