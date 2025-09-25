b48c14ac066e57df8f9284b326f162421d081a7b
RehanGeorge <rehan.george@gmail.com>
2025-09-25 12:05:20 +0530

    Add other handlers

--- main.go
+++ main.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	// Step 1: Set Content-Type
@@ -17,6 +29,25 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) getFileServerHits() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		metrics := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+		w.Write([]byte(metrics))
+	})
+}
+
+func (cfg *apiConfig) resetFileServerHits() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		metrics := fmt.Sprintf("Hits reset to 0 from %d\n", cfg.fileserverHits.Load())
+		cfg.fileserverHits.Store(0)
+		w.Write([]byte(metrics))
+	})
+}
+
 func main() {
 	fmt.Println("Hello, Chirpy Bootdev!")
 
@@ -26,11 +57,21 @@ func main() {
 	// Step 2: Use the http.FileServer to serve static files from the current directory
 	fileServer := http.FileServer(http.Dir("."))
 
-	// Use StripPrefix so that /app/index.html resolves to ./index.html
-	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	// Step 3: Initialize config for middleware
+	cfg := &apiConfig{}
 
-	// Step 3: Add readiness endpoint
+	// Use StripPrefix so that /app/index.html resolves to ./index.html
+	appHandler := http.StripPrefix("/app/", fileServer)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(appHandler))
+
+	// Step 4: Add readiness endpoint
 	mux.HandleFunc("/healthz", readinessHandler)
+
+	// Step 5: Add metrics endpoint
+	mux.Handle("/metrics", cfg.getFileServerHits())
+
+	// Step 6: Add reset metrics endpoint
+	mux.Handle("/reset", cfg.resetFileServerHits())
 
 	server := &http.Server{
 		Addr:    ":8080",
